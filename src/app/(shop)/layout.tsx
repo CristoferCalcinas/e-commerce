b63653d3d0c833c019ad82c056a7603ab10d1c76
Adrian <adrian@gmail.com>
2024-07-15 12:12:45 -0400

    Terminando el primer diseño, con un error de hidratacion

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,12 +1,15 @@
+import { Sidebar, TopMenu } from "@/components";
 
 export default function ShopLayout({
- children
+  children,
 }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
   return (
     <main className="min-h-screen bg-blue-500">
-        {children}
+      <TopMenu />
+      <Sidebar />
+      <div className="px-3 sm:px-7">{children}</div>
     </main>
   );
 }
