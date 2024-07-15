3c7465ff44a4ea738c22064cea36baa841fd3c82
Adrian <adrian@gmail.com>
2024-07-15 19:03:15 -0400

    Creando la migracion de prisma db

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,57 @@
+import { initialData } from './seed';
+import prisma from '../lib/prisma';
+
+
+
+async function main(){
+    // 1.Borrar registros de la base de datos
+    // await Promise.all()
+    await prisma.productImage.deleteMany();
+    await prisma.product.deleteMany();
+    await prisma.category.deleteMany();
+
+    const {categories, products} = initialData;
+    // Categorias 
+    const categoriesData = categories.map((category) => ({
+        name: category
+    }))
+    await prisma.category.createMany({data: categoriesData})
+
+    const categoriesDB = await prisma.category.findMany();
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+        map[category.name.toLocaleLowerCase()] = category.id;
+        return map
+    }, {} as Record<string, string>)
+    
+    // Productos
+    products.forEach(async (product) => {
+        const {type, images, ...rest} = product;
+
+        const dbProduct = await prisma.product.create({
+                data:{
+                    ...rest,
+                    categoryId: categoriesMap[type]
+                }
+             })
+        const imageData = images.map(image => ({
+            url: image,
+            productId: dbProduct.id
+        }))
+        await prisma.productImage.createMany({
+            data: imageData
+        })
+    })
+    
+
+
+
+
+
+
+    console.log("Seed ejecutado correctamente");
+} 
+
+(() => {
+    if(process.env.NODE_ENV === 'production') return;
+    main();
+})();
